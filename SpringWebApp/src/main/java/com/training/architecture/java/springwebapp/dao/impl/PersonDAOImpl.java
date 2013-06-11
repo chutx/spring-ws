1bb8f9ba760f3f3eb6c39ed3ac76b78b1efce58c
angelo <monstruito1@gmail.com>
2013-06-11 00:31:35 -0500

    the transaction is add without JNDI and mysql

--- SpringWebApp/src/main/java/com/training/architecture/java/springwebapp/dao/impl/PersonDAOImpl.java
+++ SpringWebApp/src/main/java/com/training/architecture/java/springwebapp/dao/impl/PersonDAOImpl.java
@@ -1,14 +1,21 @@
 package com.training.architecture.java.springwebapp.dao.impl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import com.training.architecture.java.springwebapp.dao.PersonDao;
+import com.training.architecture.java.springwebapp.domain.Person;
 
 @Repository
 public class PersonDAOImpl implements PersonDao {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	public String fetchName(Long id){
-		
+		Person person = entityManager.find(Person.class, id);
 		return "starting";
 	}
 
