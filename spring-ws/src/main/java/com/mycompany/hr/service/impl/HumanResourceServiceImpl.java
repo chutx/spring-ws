9908cfae2f4b07cfaa587cd21e41b9ae8dff830f
angelo <monstruito1@gmail.com>
2013-06-14 14:29:01 -0500

    web application with spring-ws

---
+++ spring-ws/src/main/java/com/mycompany/hr/service/impl/HumanResourceServiceImpl.java
@@ -0,0 +1,17 @@
+package com.mycompany.hr.service.impl;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.mycompany.hr.service.HumanResourceService;
+
+@Service(value="humanResourceService")
+public class HumanResourceServiceImpl implements HumanResourceService{
+
+	public void bookHoliday(Date startDate, Date endDate, String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
