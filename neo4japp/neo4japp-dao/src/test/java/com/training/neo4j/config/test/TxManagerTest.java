97c923ea834515901eeda0c9ea2e77f6231fd84b
angelo <monstruito1@gmail.com>
2013-10-22 23:50:27 -0500

    adding configuration for transaction manager and test case for configuration

---
+++ neo4japp/neo4japp-dao/src/test/java/com/training/neo4j/config/test/TxManagerTest.java
@@ -0,0 +1,49 @@
+package com.training.neo4j.config.test;
+
+import javax.transaction.TransactionManager;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.neo4j.graphdb.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value={
+		"classpath:META-INF/spring/tx-context-test.xml"
+})
+@TransactionConfiguration(defaultRollback=true, transactionManager="neo4jTransactionManager")
+@ActiveProfiles(value={"test"})
+public class TxManagerTest {
+
+	@Autowired
+	private JtaTransactionManager neo4jTransactionManager;
+	
+	@Autowired
+	private Neo4jTemplate neo4jTemplate; 
+	
+	@Test
+	public void txTest(){
+		TransactionManager tx = neo4jTransactionManager.getTransactionManager();
+		
+		Assert.assertNotNull(tx);
+	}
+	
+	@Test
+	@Transactional
+	@Rollback
+	public void templateTest(){
+		Node node = neo4jTemplate.createNode();
+		Assert.assertNotNull(node);
+	}
+	
+	
+}
